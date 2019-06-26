e10f10d9d987a2f70cedfb0e9b159c6a65bbadab
Artur Khylskyi <arthur.patriot@gmail.com>
2019-06-26 22:39:04 +0300

    Add Creating Table after Activating Plugin

--- includes/class-steeply-ref-activator.php
+++ includes/class-steeply-ref-activator.php
@@ -23,13 +23,33 @@
 class Steeply_Ref_Activator {
 
 	/**
-	 * Short Description. (use period)
+	 * Create Plugins Tables
 	 *
 	 * Long Description.
 	 *
-	 * @since    1.0.0
+	 * @since    1.0.3
 	 */
 	public static function activate() {
+		global $wpdb;
+
+		$table_name = $wpdb->prefix.'st_refferals';
+
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) != $table_name ) {
+
+			$create_query = "CREATE TABLE " . $table_name . " (
+	                    id mediumint(7) NOT NULL AUTO_INCREMENT,
+	                    user_id mediumint(7) NOT NULL,
+	                    ref_user_id mediumint(7) NOT NULL,
+	                    lvl tinyint(1) DEFAULT 1 NOT NULL,
+	                    PRIMARY KEY  id (id),
+	                    UNIQUE KEY id (id)
+	                    );";
+
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+			dbDelta($create_query);
+
+		}
 
 	}
 
